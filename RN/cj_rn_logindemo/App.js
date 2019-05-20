56c77aeb1b5e905d3dc76c37cda2b5667d99b59d
dvlproad <studyroad@qq.com>
2019-05-20 15:24:34 +0800

    【Feature】添加Button，用来区分rn和antd

--- RN/cj_rn_logindemo/App.js
+++ RN/cj_rn_logindemo/App.js
@@ -1,49 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import { Text, View, Alert } from 'react-native';
+// import { Button } from 'react-native';
+import { Button } from '@ant-design/react-native'
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+export default class HelloWorldApp extends Component {
+    render() {
+        return (
+            <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                <Text>Hello, world!</Text>
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+                <Button
+                    onPress={() => {
+                        Alert.alert("你点击了按钮！");
+                    }}
+                    title="normal:里面的title"
+                />
+
+                <Button disabled
+                    onPress={() => {
+                        Alert.alert("你点击了按钮！");
+                    }}
+                    title="disabled:里面的title"
+                >disabled:外面的title</Button>
+
+            </View>
+        );
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
